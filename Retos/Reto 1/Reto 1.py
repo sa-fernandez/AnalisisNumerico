ec7c6f5bcec9fe2c3d2c7879f31ef1cf0256945e
Santiago Fernandez <sa.fernandez73@gmail.com>
2021-03-13 20:42:59 -0500

    Update Reto 1.py

--- Retos/Reto 1/Reto 1.py
+++ Retos/Reto 1/Reto 1.py
@@ -1,3 +1,4 @@
+import numpy as np
 from scipy import optimize
 
 #Reto 1
@@ -14,6 +15,32 @@ def f(x):
 
 def fun(x):
     return [x[0]**2 + x[0]*x[1] - 10, x[1] + 3*x[0]*x[1]**2 - 57]
+
+def gun(x):
+    return [(x[0] + 2)**2 + (x[1] - 2)**2 - 1, x[1] + (x[0] + 2)**2 - 3]
+
+def hun(x):
+    return [4*x[0]**3 - x[1]*np.exp(x[1]), 2*x[0]*np.exp(x[0]) + x[1]**2 - 2]
+
+def aplicarMetodosNL(f, x, y):
+    try:
+        print("Resultado con la funcion fsolve = {}".format(optimize.fsolve(f, [x, y], xtol = 2**(-16))))
+    except Exception as e:
+        print("El metodo fsolve no converge")
+    try:
+        print("Resultado mediante el metodo de Newton-Krylov = {}".format(optimize.newton_krylov(f, [x, y], x_tol = 2**(-16))))
+    except Exception as e:
+        print("El metodo de Newton-Krylov no converge")
+
+    try:
+        print("Resultado mediante el metodo de Broyden 1 = {}".format(optimize.broyden1(f, [x, y], x_tol = 2**(-16))))
+    except Exception as e:
+        print("El metodo de Broyden 1 no converge")
+
+    try:
+        print("Resultado mediante el metodo de Broyden 2 = {}".format(optimize.broyden2(f, [x, y], x_tol = 2**(-50))))
+    except Exception as e:
+        print("El metodo de Broyden 2 no converge")
 
 print("Reto 1")
 print("\nPRIMER PUNTO - ALGORITMO DE BRENT")
@@ -40,26 +67,21 @@ while rep1:
 print("\nSEGUNDO PUNTO - INTERSECCION ENTRE CURVAS")
 rep2 = True
 while rep2:
-    print("\n[1] x^2 + xy = 10 [2] y + 3xy^2 = 57")
+    print("\na. [1] x^2 + xy = 10 [2] y + 3xy^2 = 57")
+    print("\nb. [1] (x + 2)^2 + (y - 2)^2 = 1 [2] y + (x + 2)^2 = 3")
+    print("\nc. [1] 4x^3 + ye^y = 0 [2] 2xe^x+y^2 - 2 = 0")
+    op = input("\nOpcion a elegir: ")
     x = float(input("\nIngrese el valor de x: "))
     y = float(input("Ingrese el valor de y: "))
-    print("Resultado con la funcion fsolve = {}".format(optimize.fsolve(fun, [x, y], xtol = 2**(-16))))
-    try:
-        print("Resultado mediante el metodo de Newton-Krylov = {}".format(optimize.newton_krylov(fun, [x, y], x_tol = 2**(-16))))
-    except Exception as e:
-        print("El metodo de Newton-Krylov no converge")
+    op.lower()
+    if op == 'a':
+        aplicarMetodosNL(fun, x, y)
+    elif op == 'b':
+        aplicarMetodosNL(gun, x, y)
+    elif op == 'c':
+        aplicarMetodosNL(hun, x, y)
 
-    try:
-        print("Resultado mediante el metodo de Broyden 1 = {}".format(optimize.broyden1(fun, [x, y], x_tol = 2**(-16))))
-    except Exception as e:
-        print("El metodo de Broyden 1 no converge")
-
-    try:
-        print("Resultado mediante el metodo de Broyden 2 = {}".format(optimize.broyden2(fun, [x, y], x_tol = 2**(-50))))
-    except Exception as e:
-        print("El metodo de Broyden 2 no converge")
-
-    rta2 = input("\nDesea evaluar otro intervalo? s(si)/n(no): ")
+    rta2 = input("\nDesea evaluar otro intervalo y/o funcion? s(si)/n(no): ")
     rta2 = rta2.lower()
     if (rta2 == 'n'):
         rep2 = False
