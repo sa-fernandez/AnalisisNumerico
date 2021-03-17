174dc4f3e8ccc77933ca643e30cf24dc11430e22
Santiago Fernandez <sa.fernandez73@gmail.com>
2021-03-17 11:56:55 -0500

    Actualización de funcionalidades del punto asignado para taller 1

--- Talleres/Taller 1/Taller Sistemas Lineales.py
+++ Talleres/Taller 1/Taller Sistemas Lineales.py
@@ -6,11 +6,7 @@ import numpy as np
 #https://numpy.org/doc/stable/reference/routines.linalg.html
 
 def evalPositivaDefinida(e):
-    for k in e:
-        if(k < 0 or isinstance(k, complex)):
-            return False
-
-    return True
+    return np.all(np.linalg.eigvals(e) > 0)
 
 def evalDiagonalDominante(a):
     d = np.diag(np.abs(a))
@@ -19,6 +15,26 @@ def evalDiagonalDominante(a):
         return True
     else:
         return False
+
+def sor(a, b, w, tol):
+    n = len(b)
+    x = np.ones(n, float)
+    r = np.zeros(n, float)
+    e = (np.linalg.norm(x - r, np.inf)) / (np.linalg.norm(x, np.inf))
+    suma = 0
+    while(e > tol):
+        for i in range(n):
+            suma = 0
+            r[i] = x[i]
+            for j in range(n):
+                if j != i:
+                    suma += a[i, j] * x[j]
+
+            x[i] = ((1 - w) * x[i]) + (w * (b[i] - suma)) / a[i, i]
+
+        e = (np.linalg.norm(x - r, np.inf)) / (np.linalg.norm(x, np.inf))
+
+    return x
 
 def gaussSeidel(a, b, tol):
     n = len(b)
@@ -41,17 +57,31 @@ def gaussSeidel(a, b, tol):
     return x
 
 print("Taller Sistemas Lineales")
+t = float(input("\nIngrese el valor de la tolerancia: "))
 rep = True
 while(rep):
-    t = float(input("\nIngrese el valor de la tolerancia: "))
     a = np.array([[10, 10**2, np.exp(0.15 * 10)], [15, 15**2, np.exp(0.15 * 15)], [20, 20**2, np.exp(0.15 * 20)]], float)
     b = np.array([25, 190, 950], float)
 
     print("\nMatriz propuesta para solución:")
     print(a)
-    print("\nArreglo solución (Gauss-Seildel):")
-    print(gaussSeidel(a, b, t))
-    rta = input("\nDesea evaluar otro sistema? s(si)/n(no): ")
+    print("\nVector de terminos independientes:")
+    print(b)
+    if evalDiagonalDominante(a) or evalPositivaDefinida(a):
+        print("\nArreglo solución (Gauss-Seildel):")
+        print(gaussSeidel(a, b, t))
+    else:
+        print("\nEl metodo de Gauss-Seidel no converge")
+
+    if evalPositivaDefinida(a):
+        print("\nArreglo solución (SOR):")
+        #print("Con valor w = {}".format(print(calculoW(a))))
+        w = float(input("\nIngrese el valor del parametro w: "))
+        print(sor(a, b, w, t))
+    else:
+        print("\nEl metodo SOR no converge")
+
+    rta = input("\nDesea evaluar otro valor de w? s(si)/n(no): ")
     rta = rta.lower()
     if (rta == 'n'):
         rep = False
