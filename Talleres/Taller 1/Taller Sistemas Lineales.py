67be74a6d017727232a84e7cb37c6ec3c8683aeb
Santiago Fernandez <sa.fernandez73@gmail.com>
2021-03-20 21:56:53 -0500

    Actualización final ejercicio taller 1

--- Talleres/Taller 1/Taller Sistemas Lineales.py
+++ Talleres/Taller 1/Taller Sistemas Lineales.py
@@ -1,9 +1,21 @@
 import numpy as np
+import sympy as sp
 
 #Taller Sistemas Lineales
 #Jose Calderon - Santiago Fernandez - Mariana Galavis - German Velasco
 #Principales referencias durante el desarrollo del codigo:
 #https://numpy.org/doc/stable/reference/routines.linalg.html
+
+def infectados(v):
+    x = sp.symbols('x')
+    f = v[0] * x+ v[1] * x **2 + v[2] * sp.exp(0.15 * x)
+    rta = 0
+    numero = 0
+    while (rta < 1500 or rta > 1600):
+        rta = f.evalf(subs={x:numero})
+        numero = numero + 1
+
+    print ("\nEl día en que los infectados es igual a {} es el numero {}".format(int(rta), numero - 1))
 
 def evalPositivaDefinida(e):
     return np.all(np.linalg.eigvals(e) > 0)
@@ -62,14 +74,15 @@ rep = True
 while(rep):
     a = np.array([[10, 10**2, np.exp(0.15 * 10)], [15, 15**2, np.exp(0.15 * 15)], [20, 20**2, np.exp(0.15 * 20)]], float)
     b = np.array([25, 190, 950], float)
-
+    
     print("\nMatriz propuesta para solución:")
     print(a)
     print("\nVector de terminos independientes:")
     print(b)
     if evalDiagonalDominante(a) or evalPositivaDefinida(a):
         print("\nArreglo solución (Gauss-Seildel):")
-        print(gaussSeidel(a, b, t))
+        v = gaussSeidel(a, b, t)
+        print(v)
     else:
         print("\nEl metodo de Gauss-Seidel no converge")
 
@@ -77,10 +90,12 @@ while(rep):
         print("\nArreglo solución (SOR):")
         #print("Con valor w = {}".format(print(calculoW(a))))
         w = float(input("\nIngrese el valor del parametro w: "))
-        print(sor(a, b, w, t))
+        v = sor(a, b, w, t)
+        print(v)
     else:
         print("\nEl metodo SOR no converge")
 
+    infectados(v)
     rta = input("\nDesea evaluar otro valor de w? s(si)/n(no): ")
     rta = rta.lower()
     if (rta == 'n'):
